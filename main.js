210a984f7595eb6f5a88fe9b8bc2a382218d18e1
senoardi <senoardi01@gmail.com>
2022-07-09 00:37:03 +0700

    upload file index.html and main.js

---
+++ main.js
@@ -0,0 +1,72 @@
+Pusher.logToConsole = true
+
+let pusher = new Pusher('a9bf317f3a67fe985c1e', {
+    cluster: 'ap1',
+    encrypted: true
+})
+
+let channel = pusher.subscribe('chat-channel')
+
+let vi = new Vue({
+    el: '#app',
+    data: {
+        url: 'http://localhost/pusher-chat/chat/',
+        chats: [],
+        username: '',
+        chatInput: '',
+        last_inserted_id: ''
+    },
+
+    methods: {
+        sendMessage: function (e) {
+
+            // console.log(e)
+            if (e.keyCode === 13 && !e.shiftKey) {
+                e.preventDefault()
+
+                if (this.chatInput == '' || this.chatInput.trim() == '')
+                    return
+
+                let date = new Date()
+                // membuat text area kosong setelah di input 
+                let themChatInput = this.chatInput
+                // membuat variabel untuk menampung id chat
+                let now = this.last_inserted_id = Date.now()
+
+
+                this.chatInput = ''
+
+                // menampilkan data lewat front end
+                this.chats.push({
+                    id: now,
+                    username: this.username,
+                    message: themChatInput,
+                    time: date.toLocaleString()
+
+                })
+
+                // menampilkan data dari server
+                axios.post(this.url + '?method=sendMessage', {
+                    id: now,
+                    username: this.username,
+                    message: themChatInput,
+                    time: date.toLocaleString(),
+                })
+
+                // .then(function (data) {
+                //     console.log(data)
+                // })
+            }
+        }
+    }
+})
+
+channel.bind('chat-event', function (data) {
+    if (data.username != null && data.id != vi.last_inserted_id)
+        vi.chats.push({
+            id: data.id,
+            username: data.username,
+            message: data.message,
+            time: data.time,
+        })
+})
